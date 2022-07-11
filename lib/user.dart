911d45ecca396eaf3ff86162874ca980e85470f5
JohnM-Kahura <Johnmungai392@gmail.com>
2022-07-11 18:38:06 +0300

    google sheets logic

---
+++ lib/user.dart
@@ -0,0 +1,8 @@
+class UserFields{
+  static final id="id";
+  static final name="name";
+  static final age="age";
+  static final email="email";
+
+  static List<String> getFields()=>[id,name,age,email];
+}
